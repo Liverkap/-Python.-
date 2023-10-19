9b931ff5d5589dc093051eb2dba659d2e6334968
Yauhen <liverkap@gmail.com>
2023-10-19 13:11:05 +0300

    add 1 solved task

---
+++ 2.1.py
@@ -0,0 +1,18 @@
+# НА EASY
+
+# a, b = int(input()), int(input())
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print((a ** 10 + b ** 10) ** 0.5)
+
+# НА EASY (ОТ ПОЛЬЗОВАТЕЛЯ)
+
+a, b = int(input()), int(input())
+
+for i in [a + b, a - b, a * b, a / b, a // b, a % b, (a ** 10 + b ** 10) ** 0.5]:
+    print(i)
