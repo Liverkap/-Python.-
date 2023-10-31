bf47a08a68a990b177341d8b7cabe9c11a820527
liverkap <liverkap@gmail.com>
2023-10-31 22:24:45 +0300

    add 1 solved task

---
+++ 2.2.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+# БОЛЬШЕ ПРЕДЫДУЩЕГО
+#
+# seq = input().split()
+#
+# count = 0
+#
+# for i in range(len(seq) - 1):
+#     current_digit = int(seq[i])
+#     next_digit = int(seq[i + 1])
+#     if current_digit < next_digit:
+#         count += 1
+#
+# print(count)
+
+
+# БОЛЬШЕ ПРЕДЫДУЩЕГО (ОТ ПРЕПОДАВАТЕЛЯ)
+
+# numbers = [int(n) for n in input().split()]
+# counter = 0
+#
+# for i in range(1, len(numbers)):
+#     if numbers[i] > numbers[i - 1]:
+#         counter += 1
+#
+# print(counter)
+
+
+# НАЗАД, ВПЕРЕД И НАОБОРОТ
