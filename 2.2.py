bbfba497ee3c27d516b6e78e0335d8248599d74b
liverkap <liverkap@gmail.com>
2023-10-31 23:01:47 +0300

    add 3 solved task

--- 2.2.py
+++ 2.2.py
@@ -35,3 +35,64 @@
 
 
 # НАЗАД, ВПЕРЕД И НАОБОРОТ
+
+# numbers = [int(n) for n in input().split()]
+#
+# for i in range(1, len(numbers), 2):
+#     numbers.insert(i-1, numbers.pop(i))
+#
+# print(*numbers)
+
+
+
+# СДВИГ В РАЗВИТИИ
+
+# numbers = [int(n) for n in input().split()]
+#
+# numbers.insert(0, numbers.pop(-1))
+#
+# print(*numbers)
+
+# СДВИГ В РАЗВИТИИ (ОТ ПРЕПОДАВАТЕЛЯ)
+
+# seq = input().split()
+# new_seq = [seq[-1]] + seq[:-1]
+#
+# print(*new_seq)
+
+
+# РАЗЛИЧНЫЕ ЭЛЕМЕНТЫ
+
+# numbers = [int(i) for i in input().split()]
+# count = 0
+# my_list = []
+#
+# for c in numbers:
+#     if c not in my_list:
+#         my_list.append(c)
+#         count += 1
+#
+# print(count)
+#
+#
+# # РАЗЛИЧНЫЕ ЭЛЕМЕНТЫ (ОТ ПРЕПОДАВАТЕЛЯ)
+#
+# numbers = input().split()
+# counter = 1
+#
+# for i in range(len(numbers) - 1):
+#     if numbers[i] != numbers[i + 1]:
+#         counter += 1
+#
+# print(counter)
+#
+#
+# # РАЗЛИЧНЫЕ ЭЛЕМЕНТЫ (ОТ ПОЛЬЗОВАТЕЛЯ)
+#
+# s=input().split()
+# a=[]
+# for i in s:
+#     if i not in a:
+#         a.append(i)
+# print(len(a))
+
