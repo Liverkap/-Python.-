5bb491b802452edf9dc87bdc559954f0ceb50742
liverkap <liverkap@gmail.com>
2023-11-02 22:43:35 +0300

    add several solved task Add new file

---
+++ 4.1.py
@@ -0,0 +1,63 @@
+#
+
+# my_list = ['планиметрия', 'стереометрия', 'задача с параметром', 'экономическая задача', 'задача на теорию чисел']
+#
+# my_list.append('уравнение')
+# print(my_list)
+#
+# del my_list[2]
+#
+# print(my_list)
+# my_list.append('неравенство')
+# print(my_list)
+#
+# del my_list[0]
+# print(my_list)
+# print(my_list[1])
+
+
+
+# конкатенация
+
+# list1 = ['a', 'b', 'c', 'd']
+# list2 = ['e', 'f', 'g']
+#
+# list3 = list1 + list2
+# print(list3)
+
+
+# zeros = [0] * 10
+# print(zeros)
+# print(len(zeros))
+
+# numbers = [4, 8, 12, 16, 34, 56, 100]
+# numbers[1:4] = [20, 24, 28]
+# print(numbers)
+
+
+# numbers = [5, 10, 15, 25]
+# print(numbers[::-2])
+
+
+# letters = ['a', 'b', 'c', 'd']
+#
+# new_letters = letters[:]
+# print(new_letters)
+# # new_letters = new_letters.copy(letters)
+# # print(new_letters)
+# new_letters = letters.copy()
+# print(new_letters)
+# # new_letters = copy(letters)
+# # print(new_letters))
+# new_letters = list(letters)
+# print(new_letters)
+#
+
+# numbers = [10, 20, 30, 40]
+# del numbers[0:6]
+# print(numbers)
+
+
+words = ['xyz', 'zara', 'beegeek']
+print(max(words))
+
