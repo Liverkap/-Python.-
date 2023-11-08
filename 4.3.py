03992b782b0d934577c6ab78775d02d8092fd4f6
liverkap <liverkap@gmail.com>
2023-11-08 23:24:28 +0300

    add new task

--- 4.3.py
+++ 4.3.py
@@ -32,13 +32,148 @@
 # print(my_list)
 
 # СЧИТЫВАЕНИЕ ВЛОЖЕННЫХ СПИСКОВ
-n = 4                                         # количество строк (элементов)
-my_list = []
+# n = 4                                         # количество строк (элементов)
+# my_list = []
+#
+# for _ in range(n):
+#     elem = [int(i) for i in input().split()]  # создаем список из элементов строки
+#     my_list.append(elem)
+#
+# print(my_list)
 
-for _ in range(n):
-    elem = [int(i) for i in input().split()]  # создаем список из элементов строки
-    my_list.append(elem)
+# ПЕРЕБОР И ВЫВОД
+
+# my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# for i in range(len(my_list)):
+#     for j in range(len(my_list[i])):
+#         print(my_list[i][j], end=' ')  # используем необязательный параметр end
+#     print()                            # перенос на новую строку
+
+
+# МОЖНО ПЕРЕБИРАТЬ САМИ ЭЛЕМЕНТЫ ВЛОЖЕННЫХ СПИСКОВ (БЕЗ ИНДЕКСОВ)
+
+# my_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# for row in my_list:
+#     for elem in row:
+#         print(elem, end=' ')
+#     print()
+
+
+#       *** ОБРАБОТКА ВЛОЖЕННЫХ ЦИКЛОВ  ******
+
+# my_list = [[1, 9, 8, 7, 4], [7, 3, 4], [2, 1]]
+#
+# total = 0
+# for i in range(len(my_list)):
+#     for j in range(len(my_list[i])):
+#         total += my_list[i][j]
+#
+# print(total)
+
+
+# my_list = [[1, 9, 8, 7, 4], [7, 3, 4], [2, 1]]
+#
+# total = 0
+# for row in my_list:
+#     for elem in row:
+#         total += elem
+#
+# print(total)
+
+# ПОДСЧЕТ С ПОМОЩЬЮ ФУНКЦИИ SUM
+# my_list = [[1, 9, 8, 7, 4], [7, 3, 4], [2, 1]]
+#
+# total = 0
+# for row in my_list:  # в один цикл
+#     total += sum(row)
+# print(total)
+
+
+# list1 = [[1, 2, 3], [4, 5]]
+# list2 = list1
+#
+# list1[0].append(7)
+#
+# print(list2)
+
+
+# n, m = int(input()), int(input())
+#
+# my_list = [[1] * m] * n
+# my_list[0][1] = 5
+# print(my_list)
+
+
+# СПИСОК ПО ОБРАЗЦУ
+
+# n = int(input())
+#
+# for _ in range(n):
+#     my_list = []
+#     for i in range(1, n + 1):
+#         my_list.append(i)
+#     print(my_list)
+
+
+# СПИСОК ПО ОБРАЗЦУ (ОТ ПРЕПОДАВАТЕЛЯ)
+
+# n = int(input())
+# result = []
+#
+# for _ in range(n):
+#     result.append(list(range(1, n + 1)))
+#
+# print(*result, sep='\n')
+
+
+# СПИСОК ПО ОБРАЗЦУ (ОТ ПОЛЬЗОВАТЕЛЯ)
+# n=int(input())
+# my_list=[[i for i in range(1,n+1)] for i in range(n)]
+# for row in my_list:
+#     print(row)
+
+
+# СПИСОК ПО ОБРАЗЦУ 2
+
+# n = int(input())
+#
+#
+# for i in range(1, n + 1):
+#     my_list = []
+#     for j in range(1, i + 1):
+#         my_list.append(j)
+#     print(my_list)
+
+# СПИСОК ПО ОБРАЗЦУ 2 (ОТ ПРЕПОДАВАТЕЛЯ)
+
+# n = int(input())
+# result = []
+#
+# for i in range(1, n + 1):
+#     result.append(list(range(1, i + 1)))
+#
+# print(*result, sep='\n')
+
+# СПИСОК ПО ОБРАЗЦУ 2 (ОТ ПОЛЬЗОВАТЕЛЯ)
+# n = int(input())
+# for j in range(1, n+1):
+#     print([i for i in range(1, j+1)])
+
+
+# ТРЕУГОЛЬНИК ПАСКАЛЯ
+
+from math import factorial
+
+from math import factorial
+n = int(input())
+
+my_list = [1] * (n + 1)
+
+
+for i in range(1, n):
+# for j in range(i):
+    my_list[i] = factorial(n) // (factorial(my_list[i]) * factorial(n - my_list[i]))
 
 print(my_list)
-
-
