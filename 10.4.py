ee5de05c38c76b59322cea216ca5924df6558f1d
liverkap <liverkap@gmail.com>
2023-12-13 21:40:55 +0300

    add one solved task add new file

---
+++ 10.4.py
@@ -0,0 +1,30 @@
+
+# СЛОВАРЬ ПРОГРАММИСТА
+
+# my_dict = {}
+#
+# for _ in range(int(input())):
+#     key, value = input().split(': ')
+#     my_dict[key.lower()] = value
+#
+# for i in range(int(input())):
+#     print(my_dict.get(input().lower(), 'Не найдено'))
+#
+#
+# # СЛОВАРЬ ПРОГРАММИСТА (ОТ ПОЛЬЗОВАТЕЛЯ)
+# d = {k.lower(): v for _ in range(int(input())) for k, v in [input().split(': ', 1)]}
+# print(*(d.get(input().lower(), 'Не найдено') for _ in range(int(input()))), sep='\n')
+
+
+# АННАГРАММЫ 1
+
+
+
+
+
+
+
+
+
+
+
