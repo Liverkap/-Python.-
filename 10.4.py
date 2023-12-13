9060ec2fd5712fa5e1717bbaff6f1198b83f384e
liverkap <liverkap@gmail.com>
2023-12-14 00:04:50 +0300

    add three solved task

--- 10.4.py
+++ 10.4.py
@@ -18,9 +18,92 @@
 
 # АННАГРАММЫ 1
 
+# a, b = input(), input()
+#
+# my_dict_a = {}
+# my_dict_b = {}
+#
+# for el in a:
+#     my_dict_a[el] = my_dict_a.get(el, 0) + 1
+#
+# for el in b:
+#     my_dict_b[el] = my_dict_b.get(el, 0) + 1
+#
+# if my_dict_a == my_dict_b:
+#     print('YES')
+# else:
+#     print('NO')
+
+# АННАГРАММЫ 1(ОТ ПОЛЬЗОВАТЕЛЯ)(ЧЕРЕЗ ЗАМЕНУ КЛЮЧА)
+# a1 = tuple(sorted(input()))
+# a2 = tuple(sorted(input()))
+# d = {a1: "NO", a2: "YES"}
+# print(d[a1])
 
 
+# АННАГРАММЫ 2
 
+# a = [el.lower().strip('.,!?:;-') for el in input().split()]
+# b = [el.lower().strip('.,!?:;-') for el in input().split()]
+#
+# my_dict_a = {}
+# my_dict_b = {}
+#
+# for i in range(len(a)):
+#     for el in a[i]:
+#         my_dict_a[el] = my_dict_a.get(el, 0) + 1
+#
+# for i in range(len(b)):
+#     for el in b[i]:
+#         my_dict_b[el] = my_dict_b.get(el, 0) + 1
+#
+# if my_dict_a == my_dict_b:
+#     print('YES')
+# else:
+#     print('NO')
+
+# АННАГРАММЫ 2 (ИСПОЛЬЗУЕМ МЕТОД JOIN)
+# a = [el.lower().strip('.,!?:;-') for el in input().split()]
+# b = [el.lower().strip('.,!?:;-') for el in input().split()]
+#
+# my_dict_a = {}
+# my_dict_b = {}
+#
+# for el in ''.join(a):
+#     my_dict_a[el] = my_dict_a.get(el, 0) + 1
+#
+# for el in ''.join(b):
+#     my_dict_b[el] = my_dict_b.get(el, 0) + 1
+#
+# if my_dict_a == my_dict_b:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# СЛОВАРЬ СИНОНИМОВ
+
+# synonyms_1 = {}
+# synonyms_2 = {}
+#
+# for _ in range(int(input())):
+#     key, value = input().split()
+#     synonyms_1[key] = value
+#     synonyms_2[value] = key
+#
+# syn = input()
+#
+# if syn in synonyms_1:
+#     print(synonyms_1[syn])
+# else:
+#     print(synonyms_2[syn])
+#
+# # СЛОВАРЬ СИНОНИМОВ (АНАЛОГИЧНО МОЕМУ РЕШЕНИЮ, НО СОБИРАЕМ ВСЕ В 1 СЛОВАРЬ)
+# words = {}
+# for _ in range(int(input())):
+#     a, b = input().split()
+#     words[a], words[b] = b, a
+# print(words[input()])
 
 
 
